0703cc24ad008cf23cc40eea5eee17c8c99aae44
vadimstrya <vadimstrya@yandex.ru>
2024-08-31 07:23:55 +0700

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    private final char[] process = new char[]{'\\', '|', '/'};
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\r load: " + process[i++]);
+                Thread.sleep(100);
+                if (i >= process.length) {
+                    i = 0;
+                }
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}

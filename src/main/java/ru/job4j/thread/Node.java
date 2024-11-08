a8724162e6340bb3912a1cb06c5f75100c795e82
vadimstrya <vadimstrya@yandex.ru>
2024-11-08 07:02:00 +0700

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/thread/Node.java
@@ -0,0 +1,20 @@
+package ru.job4j.thread;
+
+public class Node<T> {
+
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}

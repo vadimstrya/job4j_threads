9a4163f20df9fddd15808d346de29c38d4372019
vadimstrya <vadimstrya@yandex.ru>
2024-11-20 08:19:50 +0700

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.synch;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleBlockingQueueTest {
 
@@ -41,5 +43,44 @@ public class SimpleBlockingQueueTest {
         consumer.join();
 
         assertEquals(result.size(), size);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 1; index <= 5; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+
+        Assertions.assertThat(buffer).containsExactly(1, 2, 3, 4, 5);
     }
 }

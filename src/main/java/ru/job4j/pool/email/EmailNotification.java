4dce1c22fab7adfb6271c9e8e1b014f483811668
vadimstrya <vadimstrya@yandex.ru>
2024-12-24 07:10:21 +0700

    2. ExecutorService рассылка почты. [#63097 #526285]

--- src/main/java/ru/job4j/pool/email/EmailNotification.java
+++ src/main/java/ru/job4j/pool/email/EmailNotification.java
@@ -5,11 +5,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    private final ExecutorService pool;
-
-    public EmailNotification() {
-        pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    }
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         final String username = user.getUsername();
